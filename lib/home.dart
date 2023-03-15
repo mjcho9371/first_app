08670a41cb3ba5041670734476e154fb5149bfc2
mjcho9371 <127935564+mjcho9371@users.noreply.github.com>
2023-03-15 18:20:06 +0900

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -12,16 +12,51 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String keyword = '';
+  List<ProductData> allItems = [];
+  List<ProductData> items = [];
   @override
-  Widget build(BuildContext context) {
+  void initState() {
     ProductDatas datas = ProductDatas();
     ProductData data0 = datas.datas[0];
     ProductData data1 = datas.datas[1];
     ProductData data2 = datas.datas[2];
     ProductData data3 = datas.datas[3];
-    List<ProductData> items = [...datas.datas, ...datas.datas, ...datas.datas];
+    allItems = [...datas.datas, ...datas.datas, ...datas.datas, ...datas.datas];
+    items = allItems;
+    super.initState();
+  }
+
+  void onPressed() {
+    print('current keyword: ' + keyword);
+    setState(() {
+      items =
+          allItems.where((element) => element.title.contains(keyword)).toList();
+      print('current items.length: ${items.length}');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 0.3,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: onPressed,
+            ),
+          ],
+          title: TextField(
+            onChanged: (v) {
+              keyword = v;
+              print('onChanged: ' + keyword);
+            },
+          ),
+        ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -32,7 +67,7 @@ class _HomeState extends State<Home> {
                 bottom: 16,
               ),
               child: Text(
-                '민정의 마켓',
+                '지금 인기 있는 상품들',
                 style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.w600,
